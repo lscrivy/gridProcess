2f0144388ecab624f2a9f72e269b8577707bb160
Louis Scrivener <loupylouis@gmail.com>
2020-12-11 16:46:45 +0100

    Added conditional formatting to output file

--- processor.py
+++ processor.py
@@ -10,7 +10,7 @@ def process(file_path):
 	# this should ensure the smallest x / y value is 0
 	df.x = (df.x - df.x.min()).astype(int)
 	df.y = (df.y - df.y.min()).astype(int)
-	
+
 	# assuming x values start at 0...
 	result = [[None for x in range(df.x.max()+1)] for y in range(df.y.max()+1)]
 
@@ -20,4 +20,13 @@ def process(file_path):
 	# make a dataframe and then reverse the row order
 	result_df = pd.DataFrame(result)[::-1]
 
-	result_df.to_excel('results/grid.xlsx')
+	# result_df.to_excel('results/grid.xlsx', header=False, index=False)
+
+	writer = pd.ExcelWriter('results/grid.xlsx', engine='xlsxwriter')
+	result_df.to_excel(writer, sheet_name='Sheet1', header=False, index=False)
+
+	worksheet = writer.sheets['Sheet1']
+
+	worksheet.conditional_format(0,0,df.y.max(),df.x.max(), {'type': '3_color_scale'})
+
+	writer.save()
