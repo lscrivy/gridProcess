0981b0e8b36788051017c9f411ef389f0e46bb48
Louis Scrivener <loupylouis@gmail.com>
2020-12-12 14:59:17 +0100

    Added column/index labels to output

--- processor.py
+++ processor.py
@@ -4,12 +4,18 @@ def process(file_path):
 	df = pd.read_excel(file_path, usecols=[0,1,2], header=None, names=['x','y','z'])
 
 	# adjust the resolution to 1
-	df.x = (df.x/50)
-	df.y = (df.y/50)
+	df.x = (df.x/50).astype(int)
+	df.y = (df.y/50).astype(int)
+
+	# get y values for use as index later
+	y_vals = [y*50 for y in range(df.y.min(), df.y.max()+1)]
+	x_vals = [x*50 for x in range(df.x.min(), df.x.max()+1)]
+	# print(y_vals)
+	# print(x_vals)
 
 	# this should ensure the smallest x / y value is 0
-	df.x = (df.x - df.x.min()).astype(int)
-	df.y = (df.y - df.y.min()).astype(int)
+	df.x = (df.x - df.x.min())
+	df.y = (df.y - df.y.min())
 
 	# assuming x values start at 0...
 	result = [[None for x in range(df.x.max()+1)] for y in range(df.y.max()+1)]
@@ -18,15 +24,15 @@ def process(file_path):
 		result[row.y][row.x] = row.z
 
 	# make a dataframe and then reverse the row order
-	result_df = pd.DataFrame(result)[::-1]
+	result_df = pd.DataFrame(result, index=y_vals, columns=x_vals)[::-1]
 
 	# result_df.to_excel('results/grid.xlsx', header=False, index=False)
 
 	writer = pd.ExcelWriter('results/grid.xlsx', engine='xlsxwriter')
-	result_df.to_excel(writer, sheet_name='Sheet1', header=False, index=False)
+	result_df.to_excel(writer, sheet_name='Sheet1')
 
 	worksheet = writer.sheets['Sheet1']
 
-	worksheet.conditional_format(0,0,df.y.max(),df.x.max(), {'type': '3_color_scale'})
+	worksheet.conditional_format(1,1,df.y.max()+1,df.x.max()+1, {'type': '3_color_scale'})
 
 	writer.save()
