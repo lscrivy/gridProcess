729f9c5eaa93647c208c3426ff25ca0370652394
Louis Scrivener <loupylouis@gmail.com>
2020-12-16 22:12:55 +0100

    Lots of stuff added...

--- main.py
+++ main.py
@@ -1,46 +1,66 @@
 import tkinter as tk
-from tkinter import filedialog, messagebox
+from tkinter import ttk, filedialog, messagebox
 import processor
 
 class App:
 	def __init__(self, master):
 		self.master = master
-		self.frame = tk.Frame(self.master)
+		self.frame = ttk.Frame(self.master)
 		self.frame.pack()
 
 		self.file_path = ''
 
-		text = '''
-- This app converts x,y,z points to a grid
+		text = '''- This app converts x,y,z points to a grid
 - Please ensure the input file has no headers
-- The first x coordinate should be in cell A1
-		'''
-		tk.Label(self.frame, text=text, justify='left').grid(row=0,column=0,columnspan=2,padx=5)
+- The first x coordinate should be in cell A1'''
+
+		ttk.Label(self.frame, text=text, justify='left').grid(row=0,column=0,columnspan=2,padx=10,pady=(10,5))
 
 		# criteria entries
-		criteria = tk.LabelFrame(self.frame, text='Criteria')
-		criteria.grid(row=1,column=0,columnspan=2,pady=(0,10))
-		tk.Label(criteria, text='ABRfl Property A').grid(row=0,column=0,sticky='e')
-		self.crit_A = tk.Entry(criteria, width=4)
+		criteria = ttk.LabelFrame(self.frame, text='Criteria')
+		criteria.grid(row=1,column=0,columnspan=2,padx=10,pady=(0,10),sticky='nsew')
+		ttk.Label(criteria, text='ABRfl Property A: ').grid(row=0,column=0,sticky='e')
+		self.crit_A = ttk.Entry(criteria, width=4)
 		self.crit_A.grid(row=0,column=1,sticky='w')
-		tk.Label(criteria, text='ABRfl Property B').grid(row=1,column=0,sticky='e')
-		self.crit_B = tk.Entry(criteria, width=4)
+		ttk.Label(criteria, text='ABRfl Property B: ').grid(row=1,column=0,sticky='e')
+		self.crit_B = ttk.Entry(criteria, width=4)
 		self.crit_B.grid(row=1,column=1,sticky='w')
-		tk.Label(criteria, text='ABRfl Property x').grid(row=2,column=0,sticky='e')
-		self.crit_x = tk.Entry(criteria, width=4)
+		ttk.Label(criteria, text='ABRfl Property x: ').grid(row=2,column=0,sticky='e')
+		self.crit_x = ttk.Entry(criteria, width=4)
 		self.crit_x.grid(row=2,column=1,sticky='w')
 
+		ttk.Label(self.frame, text='Test name: ').grid(row=2,column=0,sticky='e')
+		self.test_name = ttk.Entry(self.frame)
+		self.test_name.grid(row=2,column=1,sticky='w')
+
+		# create two tabs
+		tab_parent = ttk.Notebook(self.frame)
+		tab_parent.grid(row=3,column=0,columnspan=2)
+		tab1 = ttk.Frame(tab_parent)
+		tab2 = ttk.Frame(tab_parent)
+		tab_parent.add(tab1, text='Processing')
+		tab_parent.add(tab2, text='Y Filter')
+
+		# tab 1
 		self.pathvar = tk.StringVar()
 		self.pathvar.set('No file selected...')
-		tk.Label(self.frame, textvariable=self.pathvar).grid(row=2,column=0,sticky='e')
-		self.select = tk.Button(self.frame, text='Select file...', command=self.get_path)
-		self.select.grid(row=2,column=1,sticky='w')
+		ttk.Label(tab1, textvariable=self.pathvar).grid(row=3,column=0,sticky='e')
+		self.select = ttk.Button(tab1, text='Select file...', command=self.get_path)
+		self.select.grid(row=3,column=1,sticky='w')
+		self.go_button = ttk.Button(tab1, text='Process data', state='disabled', command=self.process)
+		self.go_button.grid(row=4,column=0,columnspan=2,padx=5,pady=5)
 
-		self.go_button = tk.Button(self.frame, text='Process data', state='disabled', command=self.process)
-		self.go_button.grid(row=3,column=0,columnspan=2,padx=5,pady=5)
+		# tab 2
+		ttk.Label(tab2, text='y1: ').grid(row=0,column=0,sticky='e')
+		self.y1 = ttk.Entry(tab2)
+		self.y1.grid(row=0,column=1,sticky='w')
+		ttk.Label(tab2, text='y2: ').grid(row=1,column=0,sticky='e')
+		self.y2 = ttk.Entry(tab2)
+		self.y2.grid(row=1,column=1,sticky='w')
+		ttk.Button(tab2, text='Filter', command=self.filter).grid(row=2,column=0,columnspan=2,sticky='nsew')
 
 	def get_path(self):
-		self.file_path = filedialog.askopenfilename(filetypes=[('Excel files','.xlsx')])
+		self.file_path = filedialog.askopenfilename(filetypes=[('Excel','.xlsx'),('CSV','.csv')])
 		if self.file_path:
 			self.pathvar.set(self.file_path)
 			self.go_button['state'] = 'normal'
@@ -54,12 +74,31 @@ class App:
 		except:
 			messagebox.showerror('Error', 'Invalid Criteria!')
 			return
-		processor.process(self.file_path, criteria)
+		processor.process(self.file_path, criteria, self.test_name.get())
 		self.go_button['state'] = 'disabled'
 		self.pathvar.set('No file selected...')
+
+	def filter(self):
+		try:
+			y1 = int(self.y1.get())
+			y2 = int(self.y2.get())
+		except:
+			messagebox.showerror('Error', 'y values must be integers!')
+			return
+		criteria = {}
+		try:
+			criteria['PAX'] = criteria['PAY'] = float(self.crit_A.get())
+			criteria['PBX'] = criteria['PBY'] = float(self.crit_B.get())
+			criteria['PXX'] = criteria['PXY'] = float(self.crit_x.get())
+		except:
+			messagebox.showerror('Error', 'Invalid Criteria!')
+			return
+		processor.filter(criteria, self.test_name.get(), y1, y2)
 
 if __name__ == '__main__':
 	root = tk.Tk()
 	root.title('Grid Processor')
+	root.style = ttk.Style()
+	root.style.theme_use('default')
 	app = App(root)
 	root.mainloop()
--- processor.py
+++ processor.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
-def process(file_path, criteria):
-	df = pd.read_excel(file_path, usecols=[0,1,2], header=None, names=['x','y','z'])
+def process(file_path, criteria, test_name):
+	if file_path[-4:] == '.csv':
+		df = pd.read_csv(file_path, usecols=[0,1,2], header=None, names=['x','y','z'])
+	else:
+		df = pd.read_excel(file_path, usecols=[0,1,2], header=None, names=['x','y','z'])
 
 	# adjust the resolution to 1
 	df.x = (df.x/50).astype(int)
@@ -19,19 +22,23 @@ def process(file_path, criteria):
 	# both x and y are ascending
 	grid = [[None for x in range(df.x.max()+1)] for y in range(df.y.max()+1)]
 
-	for row in df.itertuples():
-		grid[row.y][row.x] = row.z
+	# using zip df.to_dict('list') is about three times faster!
+	# for row in df.itertuples():
+	# 	grid[row.y][row.x] = row.z
+
+	for row in zip(*df.to_dict('list').values()):
+		grid[row[1]][row[0]] = row[2]
 
 
 
 	# reverse the row order 
-	result_df = pd.DataFrame(grid, index=y_vals, columns=x_vals)[::-1]
+	result_df = pd.DataFrame(grid, index=y_vals, columns=x_vals)[::-1].round(2)
 
-	writer = pd.ExcelWriter('results/grid.xlsx', engine='xlsxwriter')
-	result_df.to_excel(writer, sheet_name='Grid')
+	writer = pd.ExcelWriter(f'results/{test_name} - Grid of Heights.xlsx', engine='xlsxwriter')
+	result_df.to_excel(writer, sheet_name=test_name)
 
 	# conditional formatting
-	worksheet = writer.sheets['Grid']
+	worksheet = writer.sheets[test_name]
 	worksheet.conditional_format(1,1,df.y.max()+1,df.x.max()+1, {'type': '3_color_scale'})
 
 	writer.save()
@@ -68,16 +75,68 @@ def process(file_path, criteria):
 	# y direction
 	results['PXY'] = [[roc(grid[y-6][x], val, grid[y+6][x]) if y-6>=0 and y+6<len(grid) else None for x, val in enumerate(row)] for y, row in enumerate(grid)]
 
-
+	errors = {'type':[],'count':[]}
 	for r in results:
-		frame = pd.DataFrame(results[r], index=y_vals, columns=x_vals)[::-1]
-		writer = pd.ExcelWriter(f'results/{r}.xlsx', engine='xlsxwriter')
-		frame.to_excel(writer, sheet_name='Grid')
+		frame = pd.DataFrame(results[r], index=y_vals, columns=x_vals)[::-1].round(2)
+		writer = pd.ExcelWriter(f'results/{test_name} - Property {r[1]} ({r[2].lower()}).xlsx', engine='xlsxwriter')
+		frame.to_excel(writer, sheet_name=test_name)
 		workbook = writer.book
-		worksheet = writer.sheets['Grid']
+		worksheet = writer.sheets[test_name]
 		format1 = workbook.add_format({'bg_color':'red'})
-		worksheet.conditional_format(1,1,df.y.max()+1,df.x.max()+1, {'type': 'cell', 'criteria':'not between', 'minimum':-criteria[r], 'maximum':criteria[r], 'format':format1})
+		worksheet.conditional_format(1,1,df.y.max()+1,df.x.max()+1, 
+			{'type': 'cell', 'criteria':'not between', 'minimum':-criteria[r], 'maximum':criteria[r], 'format':format1}
+			)
 
 		writer.save()
+
+		# find out how many errors there are
+		errors['type'].append(f'Property {r[1]} ({r[2].lower()})')
+		errors['count'].append(sum(val <= -criteria[r] or val >= criteria[r] for val in [item for sublist in results[r] for item in sublist] if val))
+
+	errors_df = pd.DataFrame(errors)
+	errors_df.to_excel(f'results/{test_name} - Summary.xlsx')
+
+
+def filter(criteria, test_name, y1, y2):
+	results = {}
+
+	results['grid'] = pd.read_excel(f'results/{test_name} - Grid of Heights.xlsx', index_col=0)
+	results['PAX'] = pd.read_excel(f'results/{test_name} - Property A (x).xlsx', index_col=0)
+	results['PBX'] = pd.read_excel(f'results/{test_name} - Property B (x).xlsx', index_col=0)
+	results['PXX'] = pd.read_excel(f'results/{test_name} - Property X (x).xlsx', index_col=0)
+
+	results = {n:frame.loc[frame.index.isin([y1,y2])] for n, frame in results.items()}
+	
+	for r in results:
+		if r == 'grid':
+			writer = pd.ExcelWriter(f'results/{test_name} - Grid of Heights (y={y1}or{y2}).xlsx', engine='xlsxwriter')
+			results[r].to_excel(writer, sheet_name=test_name)
+
+			# conditional formatting
+			worksheet = writer.sheets[test_name]
+			worksheet.conditional_format(1,1,2,len(results[r].columns), {'type': '3_color_scale'})
+
+			writer.save()
+		else:
+			writer = pd.ExcelWriter(f'results/{test_name} - Property {r[1]} ({r[2].lower()}) (y={y1}or{y2}).xlsx', engine='xlsxwriter')
+			results[r].to_excel(writer, sheet_name=test_name)
+			workbook = writer.book
+			worksheet = writer.sheets[test_name]
+			format1 = workbook.add_format({'bg_color':'red'})
+			worksheet.conditional_format(1,1,2,len(results[r].columns), 
+				{'type': 'cell', 'criteria':'not between', 'minimum':-criteria[r], 'maximum':criteria[r], 'format':format1}
+				)
+
+			writer.save()
+
+
+# if __name__ == '__main__':
+# 	filter({'PAX':1,'PBX':1,'PXX':1}, 'MyTest', -50, -1000)
+
+
+
+
+
+
 
 
