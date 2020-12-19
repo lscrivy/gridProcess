b5863477cdd2f8aa4af6c3b658c64d932a548461
Louis Scrivener <loupylouis@gmail.com>
2020-12-19 21:52:57 +0100

    Added more info to summary page as well as implementing summary page for y filter function

--- processor.py
+++ processor.py
@@ -75,7 +75,7 @@ def process(file_path, criteria, test_name):
 	results['PXY'] = [[roc(grid[y-6][x], val, grid[y+6][x]) if y-6>=0 and y+6<len(grid) else None for x, val in enumerate(row)] for y, row in enumerate(grid)]
 
 	# to count the errors for our summary file
-	errors = {'Type':[],'Criteria':[],'Error Count':[]}
+	errors = {'Type':[],'Criteria':[],'Total Points':[],'Non-compliant Points':[],'Non-compliance %':[]}
 	for r in results:
 		frame = pd.DataFrame(results[r], index=y_vals, columns=x_vals)[::-1].round(2)
 		sheet_name = f'ABRfl Property {r[1]} ({r[2].lower()})'
@@ -90,9 +90,15 @@ def process(file_path, criteria, test_name):
 		# find out how many errors there are
 		errors['Type'].append(f'ABRfl Property {r[1]} ({r[2].lower()})')
 		errors['Criteria'].append(f'-{criteria[r]} ≤ z ≤ {criteria[r]}')
-		errors['Error Count'].append(sum(val <= -criteria[r] or val >= criteria[r] for val in [item for sublist in results[r] for item in sublist] if val))
+		total_points = sum(len([z for z in row if z]) for row in results[r])
+		errors['Total Points'].append(total_points)
+		error_count = sum(val <= -criteria[r] or val >= criteria[r] for val in [item for sublist in results[r] for item in sublist] if val)
+		errors['Non-compliant Points'].append(error_count)
+		errors['Non-compliance %'].append(round((error_count/total_points)*100 if total_points else 0, 2))
+
 
 	errors_df = pd.DataFrame(errors)
+
 
 	# summary file
 	errors_df.to_excel(writer, sheet_name='Summary', index=False, startrow=3)
@@ -102,7 +108,7 @@ def process(file_path, criteria, test_name):
 
 	worksheet.write(0, 0, test_name)
 	worksheet.write(2, 0, 'Errors')
-	worksheet.set_column(0,2,20)
+	worksheet.set_column(0,4,20)
 
 	writer.save()
 
@@ -118,6 +124,9 @@ def filter(criteria, test_name, y1, y2):
 
 	results = {n:frame.loc[frame.index.isin([y1,y2])] for n, frame in results.items()}
 	writer = pd.ExcelWriter(f'results/{test_name} (y={y1}|{y2}).xlsx')
+
+	# to count the errors for our summary file
+	errors = {'Type':[],'Criteria':[],'Total Points':[],'Non-compliant Points':[],'Non-compliance %':[]}
 
 	for r in results:
 		if r == 'grid':
@@ -135,6 +144,27 @@ def filter(criteria, test_name, y1, y2):
 			worksheet.conditional_format(1,1,2,len(results[r].columns), 
 				{'type': 'cell', 'criteria':'not between', 'minimum':-criteria[r], 'maximum':criteria[r], 'format':format1}
 				)
+
+			errors['Type'].append(f'ABRfl Property {r[1]} ({r[2].lower()})')
+			errors['Criteria'].append(f'-{criteria[r]} ≤ z ≤ {criteria[r]}')
+			total_points = results[r].count().sum()
+			errors['Total Points'].append(total_points)
+			error_count = results[r].where((results[r] <= -criteria[r]) | (results[r] >= criteria[r])).count().sum()
+			# error_count = sum(val <= -criteria[r] or val >= criteria[r] for val in [item for sublist in results[r] for item in sublist] if val)
+			errors['Non-compliant Points'].append(error_count)
+			errors['Non-compliance %'].append(round((error_count/total_points)*100 if total_points else 0, 2))
+
+	errors_df = pd.DataFrame(errors)
+
+	# summary file
+	errors_df.to_excel(writer, sheet_name='Summary', index=False, startrow=3)
+
+	workbook = writer.book
+	worksheet = writer.sheets['Summary']
+
+	worksheet.write(0, 0, test_name)
+	worksheet.write(2, 0, 'Errors')
+	worksheet.set_column(0,4,20)
 
 	writer.save()
 
